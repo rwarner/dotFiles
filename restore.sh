b304e916c6603dfa60e7913d7419669f5dc458da
Ryan Warner <kentoe@gmail.com>
2018-03-16 11:59:35 -0400

    Adds git ignore to git config via restore.sh

--- restore.sh
+++ restore.sh
@@ -7,6 +7,7 @@ cp vimrc ~/.vimrc
 
 # Git restoration
 cp gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 
 # Bash restoration
 cp bash/bash_aliases ~/.bash_aliases
