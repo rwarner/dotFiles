bfafb71c48072b174fb81fe0ca070b1b6123f09b
Ryan Warner <kentoe@gmail.com>
2018-03-16 12:43:44 -0400

    Adds check for linux/osx on restoration

--- restore.sh
+++ restore.sh
@@ -22,8 +22,23 @@ git config --global core.excludesfile ~/.gitignore_global
 cp bash/bash_aliases ~/.bash_aliases
 cp bash/bash_profile ~/.bash_profile
 
-# iTerm2 -> Preferences -> General has an auto-save to directory
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$PWD/iTerm2"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+if [ "$(uname)" == "Darwin" ]; then
+    # OS X specific 
+    
+    # Seperating the iTerm2 loading since Linux wouldn't utilize this
+
+    # iTerm2 -> Preferences -> General has an auto-save to directory
+    # Specify the preferences directory
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$PWD/iTerm2"
+    # Tell iTerm2 to use the custom preferences in the directory
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+    #Force iTerm2 to read new settings
+    defaults read com.googlecode.iterm2
+
+    echo "Please restart iTerm2, if executing a restore from iTerm2"
+elif [ "$(uname)" == "Linux" ]; then
+    #Linux specific
+
+    #elif block can't be empty
+    echo "No iTerm2 on Linux, ignoring"
+fi
